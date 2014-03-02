b4cd1d73cdee4dd64dd4c61563a571943815ce45
Lillian Ng <lillian.k.ng@gmail.com>
2014-03-02 11:44:07 -0800

    ch5 code

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,8 +1,28 @@
 class QuotesController < Rulers::Controller
+  def index
+    quotes = FileModel.all
+    render :index, quotes: quotes
+  end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth one k pixels",
+      "attribution" => "Me"
+    }
+    m = FileModel.create attrs
+    render :quote, obj: m
+  end
+
   def a_quote
     @adjective = "fine"
     @noun = "coffee"
     render :a_quote
+  end
+
+  def quote_1
+    quote_1 = Rulers::Model::FileModel.find(1)
+    render :quote, obj: quote_1
   end
 
   def exception
