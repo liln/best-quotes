a30b1be0ac959ead99ab707c95c970d1f1b8d3ae
Lillian Ng <lillian.k.ng@gmail.com>
2014-03-02 17:48:48 -0800

    ch5 exercises

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -14,10 +14,25 @@ class QuotesController < Rulers::Controller
     render :quote, obj: m
   end
 
+  def update_quote
+    quote = Rulers::Model::FileModel.find(3)
+    quote["quote"] = "A picture is worth one thousand pixels."
+    if quote.save
+      render :quote, obj: quote
+    else
+      # some error :(
+    end
+  end
+
   def a_quote
     @adjective = "fine"
     @noun = "coffee"
     render :a_quote
+  end
+
+  def submitter_bob
+    quotes = FileModel.find_all_by_submitter("Bob")
+    render :index, quotes: quotes
   end
 
   def quote_1
